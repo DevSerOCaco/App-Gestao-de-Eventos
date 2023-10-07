26058465cf946ea69cba6fe936d187d3962ce646
Paulo Ricardo Silva dos Santos Filho <pauloricardossfilho@gmail.com>
2023-10-07 13:59:38 -0300

    Primeiro commit base spring Maven

---
+++ src/main/java/com/techchallenge/appgestaodeeventos/AppGestaoDeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.techchallenge.appgestaodeeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestaoDeEventosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppGestaoDeEventosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techchallenge/appgestaodeeventos/AppGestaoDeEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.appgestaodeeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGestaoDeEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
